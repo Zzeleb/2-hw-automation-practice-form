25408fffa3b7ae91a3efbd68637bbd42106cecc0
Zzeleb <zzeleb@gmail.com>
2022-09-20 21:45:05 +0300

    added tests exept last one

--- src/test/java/com/demoqa/AutomationPracticeFormTests.java
+++ src/test/java/com/demoqa/AutomationPracticeFormTests.java
@@ -4,7 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class AutomationPracticeFormTests {
 
@@ -17,6 +18,46 @@ public class AutomationPracticeFormTests {
 
     @Test
     void fillFormTest() {
+        String name = "Artem";
+        String surname = "Zeleb";
+        String Email = "zeleb@inbox.ru";
+        String Number = "9042545658";
+        String picture = "123.jpg";
+        String subjects = "Maths";
+        String hobby = "Reading";
+        String address = "Nevskiy 5, 15";
+
         open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+        $("#firstName").setValue(name);
+        $("#lastName").setValue(surname);
+        $("#userEmail").setValue(Email);
+        $("#gender-radio-1").parent().click();
+        $("#userNumber").setValue(Number);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption("1985");
+        $(".react-datepicker__month-select").selectOption("October");
+        $(".react-datepicker__day--016").click();
+        $("#subjectsInput").setValue(subjects).pressEnter();
+        $$(".custom-control-label").findBy(text(hobby)).click();
+        $("input#uploadPicture").uploadFromClasspath(picture);
+        $("#currentAddress").setValue(address);
+        //$$("#stateCity-wrapper").findBy(text("Haryana")).click();
+        //$(".css-1g6gooi").selectOptionContainingText("Haryana");
+        //$("#city").selectOptionContainingText("Karnal");
+
+        $("#submit").click();
+
+        $(".table-responsive").shouldHave(text(name));
+        $(".table-responsive").shouldHave(text(surname));
+        $(".table-responsive").shouldHave(text(Email));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text(Number));
+        $(".table-responsive").shouldHave(text("16 October,1985"));
+        $(".table-responsive").shouldHave(text(subjects));
+        $(".table-responsive").shouldHave(text(hobby));
+        $(".table-responsive").shouldHave(text(picture));
+        $(".table-responsive").shouldHave(text(address));
     }
 }
