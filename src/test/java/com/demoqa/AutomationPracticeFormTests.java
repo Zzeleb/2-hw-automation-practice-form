c08911b39c826569794ce0ff41017fc79ed34845
Zzeleb <zzeleb@gmail.com>
2022-09-20 18:06:14 +0300

    added class AutomationPracticeFormTests browser config

---
+++ src/test/java/com/demoqa/AutomationPracticeFormTests.java
@@ -0,0 +1,22 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class AutomationPracticeFormTests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+    }
+}
